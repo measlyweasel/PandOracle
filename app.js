ac1a83608a3227fcce7823dd2987fee4a7bcb63d
jason <jfcahoon@gmail.com>
2013-02-23 16:23:27 -0600

    notifications and updating of songs list in app.js

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
-chrome.webRequest.onCompleted.addListener(
+function Song(url){
+	this.url = url;
+}
+Song.prototype.url = "";
+Song.prototype.title="";
+Song.prototype.artist="";
+Song.prototype.album="";
+Song.prototype.art="";
+Song.prototype.toString = function(){
+	return this.title+"-"+this.artist+"["+this.album+"]";
+}
+
+var songs = []
+
+var notification = webkitNotifications.createNotification(
+	"icon.png",
+	"Title",
+	"Body"
+)
+
+function processSongMeta(songMeta){
+
+	songs[songs.length-1].title = songMeta.title;
+	songs[songs.length-1].artist = songMeta.artist;
+	songs[songs.length-1].album = songMeta.album;
+	songs[songs.length-1].art = songMeta.art;
+
+	notification.cancel();
+	notification = webkitNotifications.createNotification(
+		songMeta.art,
+		songMeta.title + " - " + songMeta.artist,
+		"Download"
+	);
+	notification.show();
+}
+
+chrome.webRequest.onResponseStarted.addListener(
 	function(details) {
   		console.log("completed: " + details);
-		console.log("completed: " + info.url);
+		console.log("completed: " + details.url);
+
+		songs.push(new Song(details.url));
 	},
 	{ 	
 		urls: ["*://*.pandora.com/access/*"]
-		//types: ['xmlhttprequest']
+	}
+);
+
+chrome.extension.onMessage.addListener(
+	function(songMeta,sender,sendResponse){
+		processSongMeta(songMeta);
 	}
 );
--- pandoracle.js
+++ pandoracle.js
@@ -12,12 +12,13 @@ var prevSong = new Song();
 var getSongMeta = function getSongMeta() {
 	try {
 		var song = new Song();
-		song.title = document.getElementsByClassName("songTitle")[0].innerHTML;
-		song.artist = document.getElementsByClassName("artistSummary")[0].innerHTML;
-		song.album = document.getElementsByClassName("albumTitle")[0].innerHTML;
+		song.title = document.getElementsByClassName("songTitle")[0].text;
+		song.artist = document.getElementsByClassName("artistSummary")[0].text;
+		song.album = document.getElementsByClassName("albumTitle")[0].text;
 		song.art = document.getElementsByClassName("playerBarArt")[0].src;
 
-		if(song.toString() != prevSong.toString()) {
+		if(song.toString() != prevSong.toString()
+			&& song.art != prevSong.art) {
 			prevSong = song
 			chrome.extension.sendMessage(song);
 		}
@@ -25,7 +26,7 @@ var getSongMeta = function getSongMeta() {
 	catch(err) { }	
 };
 
-window.setInterval(getSongMeta,250);
+window.setInterval(getSongMeta,500);
 
 // chrome.extension.onMessage.addListener(
 // 	function(request, sender, sendResponse){
