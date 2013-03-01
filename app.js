9fcb9d894935c6a6573a682268a18b7bfc3d8fba
jason <jfcahoon@gmail.com>
2013-03-01 14:27:14 -0600

    improved chrome mainline and windows compatability

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ Song.prototype.title="";
 Song.prototype.artist="";
 Song.prototype.album="";
 Song.prototype.art="";
+Song.prototype.tabId = 0;
 Song.prototype.toString = function(){
 	return this.title+" - "+this.artist+"["+this.album+"]";
 }
 
-var songs = []
+var songs = [];
+var tabId = 0;
 
 var notification = webkitNotifications.createNotification(
 	"icon.png",
@@ -19,23 +21,31 @@ var notification = webkitNotifications.createNotification(
 )
 
 function downloadSong(song) {
-	chrome.downloads.download(
+
+	if(chrome.downloads){
+		chrome.downloads.download(
 		{
 			url: song.url,
 			filename: song.toString() + ".mp4",
 			saveAs: true
 		},
 		function(downloadId){ /*download callback*/	}
-	);
+		);		
+	} else { //no chrome download api
+		var msg = {"type": "dl", "song":song};
+		chrome.tabs.sendMessage(song.tabId,msg);
+	}
+
 	notification.cancel()
 }
 
-function processSongMeta(songMeta){
+function processSongMeta(songMeta, tabId){
 	var songIndex = songs.length-1;
 	songs[songIndex].title = songMeta.title;
 	songs[songIndex].artist = songMeta.artist;
 	songs[songIndex].album = songMeta.album;
 	songs[songIndex].art = songMeta.art;
+	songs[songIndex].tabId = tabId;
 
 	notification.cancel();
 	notification = webkitNotifications.createNotification(
@@ -59,7 +69,9 @@ chrome.webRequest.onResponseStarted.addListener(
 );
 
 chrome.extension.onMessage.addListener(
-	function(songMeta,sender,sendResponse){
-		processSongMeta(songMeta);
+	function(msg,sender,sendResponse){
+		if(msg.type=="songMeta"){
+			processSongMeta(msg.songMeta, sender.tab.id);
+		}
 	}
 );
--- pandoracle.js
+++ pandoracle.js
@@ -20,10 +20,32 @@ var getSongMeta = function getSongMeta() {
 		if(song.toString() != prevSong.toString()
 			&& song.art != prevSong.art) {
 			prevSong = song
-			chrome.extension.sendMessage(song);
+			var msg  = {"type":"songMeta", "songMeta": song};
+			chrome.extension.sendMessage(msg);
 		}
 	} 
 	catch(err) { }	
 };
 
 window.setInterval(getSongMeta,500);
+
+
+function addDownloadLink(song){
+	var downloadLink = document.createElement('a');
+	downloadLink.href = song.url;
+	downloadLink.download = song.title+"-"+song.artist+"["+song.album+"]"+ ".mp4";
+	var click = document.createEvent("MouseEvents");
+	click.initMouseEvent(
+		"click", true, false, self, 0, 0, 0, 0, 0
+		, false, false, false, false, 0, null
+	);
+	downloadLink.dispatchEvent(click);
+}
+
+chrome.extension.onMessage.addListener(
+	function(msg,sender,sendResponse){
+		if(msg.type == "dl"){
+			addDownloadLink(msg.song);
+		}		
+	}
+);
