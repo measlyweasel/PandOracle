a1d5d4656f21bf81ac28590096633460a4d78391
Doug Royal <email@dougroyal.com>
2014-11-30 13:54:13 -0600

    minor tweaks

--- app.js
+++ app.js
@@ -1,77 +1,19 @@
-function Song(url){
-	this.url = url;
-}
-Song.prototype.url = "";
-Song.prototype.title="";
-Song.prototype.artist="";
-Song.prototype.album="";
-Song.prototype.art="";
-Song.prototype.tabId = 0;
-Song.prototype.toString = function(){
-	return this.title+" - "+this.artist+"["+this.album+"]";
-}
+var url = null;
 
-var songs = [];
-var tabId = 0;
-
-var notification = webkitNotifications.createNotification(
-	"icon.png",
-	"Title",
-	"Body"
-)
-
-function downloadSong(song) {
-
-	if(chrome.downloads){
-		chrome.downloads.download(
-		{
-			url: song.url,
-			filename: song.toString() + ".mp4",
-			saveAs: true
-		},
-		function(downloadId){ /*download callback*/	}
-		);		
-	} else { //no chrome download api
-		var msg = {"type": "dl", "song":song};
-		chrome.tabs.sendMessage(song.tabId,msg);
-	}
-
-	notification.cancel()
-}
-
-function processSongMeta(songMeta, tabId){
-	var songIndex = songs.length-1;
-	songs[songIndex].title = songMeta.title;
-	songs[songIndex].artist = songMeta.artist;
-	songs[songIndex].album = songMeta.album;
-	songs[songIndex].art = songMeta.art;
-	songs[songIndex].tabId = tabId;
-
-	notification.cancel();
-	notification = webkitNotifications.createNotification(
-		songMeta.art,
-		songMeta.title + " - " + songMeta.artist,
-		"Download"
-	);
-	notification.onclick = function() { downloadSong(songs[songIndex]); }
-	notification.show();
-}
-
+/* Watch for messages from browser to pandora and capture the song URL. */
 chrome.webRequest.onResponseStarted.addListener(
-	function(details) {
-		if(songs.length==0 || details.url!=songs[songs.length-1].url){
-			songs.push(new Song(details.url));
-		}
-	},
-	{ 	
-		urls: ["*://*.pandora.com/access/*"]
-	}
+    function(details) {
+        url = details.url;
+    },
+    { 	
+        urls: ["*://*.pandora.com/access/*"]
+    }
 );
 
+/* Handle messages from pandoracle.js when the download button is clicked. */
+/* send them the most recently captured song url */
 chrome.extension.onMessage.addListener(
-	function(msg,sender,sendResponse){
-		if(msg.type=="songMeta"){
-			processSongMeta(msg.songMeta, sender.tab.id);
-		}
-	}
+    function(msg,sender,sendResponse){
+        sendResponse(url);
+    }
 );
--- pandoracle.js
+++ pandoracle.js
@@ -4,48 +4,39 @@ Song.prototype.artist="";
 Song.prototype.album="";
 Song.prototype.art="";
 Song.prototype.toString = function(){
-	return this.title+"-"+this.artist+"["+this.album+"]";
+    return this.title+"-"+this.artist+"["+this.album+"]";
 }
 
-var prevSong = new Song();
+var song = null;
+
+$(document).ready(function() {
+    var $downloadButton = $($.parseHTML('<div class="button btn_bg"><div class="text">Download</div></div>'));
+    $downloadButton.click(downloadSong);
+    $('#trackInfoButtons .buttons').append($downloadButton);
+});
+
+var downloadSong = function() {
+    chrome.runtime.sendMessage({'can haz':'d url?'}, function(url) {
+        var fileName = song.title+"-"+song.artist+"["+song.album+"]"+ ".mp4";
+        // Chrome's 'download' attribute support is borked right now, so I'm dumping this in the console ... then copy, rename, pasta.
+        console.log(fileName);
+
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        a.click();
+    });
+}
 
 var getSongMeta = function getSongMeta() {
 	try {
-		var song = new Song();
+		song = new Song();
 		song.title = document.getElementsByClassName("songTitle")[0].text;
 		song.artist = document.getElementsByClassName("artistSummary")[0].text;
 		song.album = document.getElementsByClassName("albumTitle")[0].text;
 		song.art = document.getElementsByClassName("playerBarArt")[0].src;
-
-		if(song.toString() != prevSong.toString()
-			&& song.art != prevSong.art) {
-			prevSong = song
-			var msg  = {"type":"songMeta", "songMeta": song};
-			chrome.extension.sendMessage(msg);
-		}
 	} 
 	catch(err) { }	
 };
 
 window.setInterval(getSongMeta,500);
-
-
-function addDownloadLink(song){
-	var downloadLink = document.createElement('a');
-	downloadLink.href = song.url;
-	downloadLink.download = song.title+"-"+song.artist+"["+song.album+"]"+ ".mp4";
-	var click = document.createEvent("MouseEvents");
-	click.initMouseEvent(
-		"click", true, false, self, 0, 0, 0, 0, 0
-		, false, false, false, false, 0, null
-	);
-	downloadLink.dispatchEvent(click);
-}
-
-chrome.extension.onMessage.addListener(
-	function(msg,sender,sendResponse){
-		if(msg.type == "dl"){
-			addDownloadLink(msg.song);
-		}		
-	}
-);
